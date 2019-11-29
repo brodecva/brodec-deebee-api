5a21c70eeb7159bccc761ee5872c8d1048ce973e
Václav Brodec <brodecva@gmail.com>
2019-11-29 14:12:54 +0100

    Forgotten comments removal

--- brodec-deebee-api/src/main/java/net/brodec/sandbox/deebee/Application.java
+++ brodec-deebee-api/src/main/java/net/brodec/sandbox/deebee/Application.java
@@ -1,17 +1,12 @@
 package net.brodec.sandbox.deebee;
 
-//import java.text.SimpleDateFormat;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-//import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
@@ -26,7 +21,7 @@ public class Application {
 	public Jackson2ObjectMapperBuilder jacksonBuilder() {
 		Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
 		builder.indentOutput(true);
-		//builder.dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+		
 		return builder;
 	}
 }
